bd09c487b0cdb51c613038a912cdec241ef8b1e0
Siheng Zhang <sihengzh@usc.edu>
2024-11-02 07:44:11 -0700

    openMP added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <spdlog/spdlog.h>
 #include <tiny_obj_loader.h>
+#include <omp.h>
 #include <iostream>
 
 int main() {
+    //just for testing included libraries
     tinyobj::attrib_t attrib;
     spdlog::info("Hello, world!");
+#pragma omp parallel
+    {
+        const int thread_id = omp_get_thread_num();
+        spdlog::info("Hello from thread " + std::to_string(thread_id));
+    }
     return 0;
 }
